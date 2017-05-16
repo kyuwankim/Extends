4d5dca838432ca9634fecf0e7857e28e4fe204ee
kyuwankim <lpnids@gmail.com>
2017-05-16 14:55:05 +0900

    java.lang.ArrayIndexOutOfBoundsException 예제

--- src/com/kyuwan/java/exception/ExceptionMain.java
+++ src/com/kyuwan/java/exception/ExceptionMain.java
@@ -4,8 +4,11 @@ public class ExceptionMain {
 
 	public static void main(String[] args) {
 
+		// java.lang.ArrayIndexOutOfBoundsException ¿¹ 
 		int array[] = new int[6];
 		
+		array[5] = 10;
+		array[6] = 11;
 		
 	}
 
