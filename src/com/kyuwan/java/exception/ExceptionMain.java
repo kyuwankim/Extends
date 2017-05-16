3f8a70dcccfe2bc01df10e56b30b98bae089485d
kyuwankim <lpnids@gmail.com>
2017-05-16 15:03:14 +0900

    Finally 추가

--- src/com/kyuwan/java/exception/ExceptionMain.java
+++ src/com/kyuwan/java/exception/ExceptionMain.java
@@ -3,18 +3,23 @@ package com.kyuwan.java.exception;
 public class ExceptionMain {
 
 	public static void main(String[] args) {
-
-		// java.lang.ArrayIndexOutOfBoundsException 예 
-		try{
-		int array[] = new int[6];
 		
-		array[5] = 10;
-		array[6] = 11;
-		}catch(ArrayIndexOutOfBoundsException e){
+		int array[] = new int[6];
+
+		// java.lang.ArrayIndexOutOfBoundsException 예
+		try {
+
+			array[0] = 5;
+			array[5] = 10;
+			// 에러가 발생하더라도 결과값을 리턴해야한다
+			array[6] = 11;
+		} catch (ArrayIndexOutOfBoundsException e) {
 			e.printStackTrace();
+		} finally {
+			// Try-catch 를 통해 문제가 생겨도 일단은 진행을 시켜라
+			System.out.println("다섯번째값은 : " + array[5]);
 		}
-		// Try-catch 를 통해 문제가 생겨도 일단은 진행을 시켜라 
-		System.out.println("시스템이 완료되었습니다" );
+		System.out.print("시스템이 완료되었습니다");
 	}
 
 }
