53cd3938ddd56bef95674a2aa5ccd8c5b924f38e
kyuwankim <lpnids@gmail.com>
2017-05-16 11:54:20 +0900

    추상클래스 내용 추가

--- src/com/kyuwan/java/ExtendMain.java
+++ src/com/kyuwan/java/ExtendMain.java
@@ -7,18 +7,9 @@ public class ExtendMain extends Father {
 
 	public static void main(String[] args) {
 
-		Father fa = new Father();
-		fa.lastname = "Doe";
-		fa.name = "John";
-		fa.car = "Car";
-		fa.money = 1000000000;
-		
-		Son son = new Son();
-		son.girlfriend = "Scott";
-
-		son.goDesti();
+	Father father = new Father();
+	father.process();
 	
-
 	}
 
 }
--- src/com/kyuwan/java/obj/Father.java
+++ src/com/kyuwan/java/obj/Father.java
@@ -3,78 +3,77 @@ package com.kyuwan.java.obj;
 public class Father extends Fix implements Cook {
 	public String lastname;
 	public String name;
-	
+
 	public int age;
 	public int money;
-	
+
 	public String house;
 	public String car;
 	public String wife;
-	
-	public void process9(){
-		// 코드 9줄 
+
+	public void process9() {
+		// 코드 9줄
 	}
-	
-	public void goDesti(){
+
+	public void goDesti() {
 
 		process9();
-		
+
 		System.out.println("9줄의 결과 출력");
-	
+
 	}
 
 	@Override
 	public void boil() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void grill() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void fry() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void oven() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void steam() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void disassemble() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("분해완료");
+
 	}
 
 	@Override
 	public void replacePart() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("교체완료");
+
 	}
 
 	@Override
 	public void clean() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("청소완료");
+
 	}
 
 	@Override
 	public void assemble() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("조립완료");
 	}
-	
+
 }
