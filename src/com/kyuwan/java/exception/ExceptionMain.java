01318b6eb729c8c2c1fb8513dae9fe734301888b
kyuwankim <lpnids@gmail.com>
2017-05-16 15:00:27 +0900

    Try Catch 문 추가

--- src/com/kyuwan/java/exception/ExceptionMain.java
+++ src/com/kyuwan/java/exception/ExceptionMain.java
@@ -5,11 +5,16 @@ public class ExceptionMain {
 	public static void main(String[] args) {
 
 		// java.lang.ArrayIndexOutOfBoundsException 예 
+		try{
 		int array[] = new int[6];
 		
 		array[5] = 10;
 		array[6] = 11;
-		
+		}catch(ArrayIndexOutOfBoundsException e){
+			e.printStackTrace();
+		}
+		// Try-catch 를 통해 문제가 생겨도 일단은 진행을 시켜라 
+		System.out.println("시스템이 완료되었습니다" );
 	}
 
 }
